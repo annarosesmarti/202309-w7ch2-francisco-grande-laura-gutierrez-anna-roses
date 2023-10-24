aadadd11f728b35fceab6dc120b3352c6eaf50b6
Laura Gutierrez <lauragsanahuja@gmail.com>
2023-10-24 23:25:04 +0200

    Add getCharacterImage function and testing

--- src/components/getCharacterImage/getCharacterImage.tsx
+++ src/components/getCharacterImage/getCharacterImage.tsx
@@ -1,11 +1,18 @@
 interface CharacterImageProps {
   characterId: number;
-};
+  altText?: string;
+}
 
-function getImageData({ characterId, altText }: CharacterImageProps): JSX.Element {
+function getImageData({ characterId }: CharacterImageProps): JSX.Element {
   const imageUrl = `https://starwars-visualguide.com/assets/img/characters/${characterId}.jpg`;
 
-  return <img src={ imageUrl } alt={ `This character is ${characterId}` className="character_image" } />;
-};
+  return (
+    <img
+      src={imageUrl}
+      alt={`This character is ${characterId}`}
+      className="character_image"
+    />
+  );
+}
 
 export default getImageData;
