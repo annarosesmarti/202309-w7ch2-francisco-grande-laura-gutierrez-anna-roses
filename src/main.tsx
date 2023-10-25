702059391fd1235980b19b68f1fa809069281403
Laura Gutierrez <lauragsanahuja@gmail.com>
2023-10-25 06:51:07 +0200

    Fix bug in importing App to main.tsx file

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import CharacterCard from "./components/CharacterCard/CharacterCard.js";
+import getCharacterImage from "./components/getCharacterImage/getCharacterImage.js";
+import App from "./components/App/App.js";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <div />
-  </React.StrictMode>
+    <App />
+  </React.StrictMode>,
 );
