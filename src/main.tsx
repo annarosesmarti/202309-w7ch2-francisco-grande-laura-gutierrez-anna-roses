7d296840177b9416342a540c60f1a41cf0dd4fee
Laura Gutierrez <lauragsanahuja@gmail.com>
2023-10-25 06:53:45 +0200

    Fix createRoot method's content

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,5 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import CharacterCard from "./components/CharacterCard/CharacterCard.js";
-import getCharacterImage from "./components/getCharacterImage/getCharacterImage.js";
 import App from "./components/App/App.js";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
