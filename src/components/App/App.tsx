2f076c639830665306f8efd1e9884c06a9e1f298
Laura Gutierrez <lauragsanahuja@gmail.com>
2023-10-26 11:02:22 +0200

    Add resetMass function

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,14 +1,26 @@
 import Button from "../Button/Button";
 import CharacterList from "../CharacterList/CharacterList";
+import "../Button/Button.css";
+import { useState } from "react";
 
 const App = (): React.ReactElement => {
+  const [number, setNumber] = useState(0);
+
+  const resetMass = () => {
+    if (number >= 1) {
+      return;
+    }
+
+    setNumber(0);
+  };
+
   return (
     <div className="app-container">
       <header className="main-header">
         <h1 className="main-header__title">STAR WARS</h1>
         <Button
-          className="button__reset-mass"
-          actionOnClick={() => {}}
+          className="card__button"
+          actionOnClick={resetMass}
           text="Reset mass"
         />
         <CharacterList />
